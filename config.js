96f1adf76774d1a5a0b4795d3f5289e44bfdaff0
Ratheesh K R <ratheesh.kr@netspective.org>
2018-04-30 14:51:41 +0530

    Deleted Unwanted files from the repo

---
+++ config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: 'buAMJ57EOfnCmllEaxdTf4fZA',
+    consumersecret: 'wcvWRm22MGwXoweWk9hRWlHxmUNtqZyGHzNg645Ua4g1b0oMHz',
+    bearertoken: ''
+};
+ 
+module.exports = appsettings;
--- server/config.js
+++
@@ -1,7 +0,0 @@
-var appsettings = {
-    consumerkey: 'buAMJ57EOfnCmllEaxdTf4fZA',
-    consumersecret: 'wcvWRm22MGwXoweWk9hRWlHxmUNtqZyGHzNg645Ua4g1b0oMHz',
-    bearertoken: ''
-};
- 
-module.exports = appsettings;
